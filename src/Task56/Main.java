64b352ab19a01c392881463d38289d3b430a132a
Stanimir <stpetkov4@gmail.com>
2016-05-25 22:53:48 +0300

    String

--- src/Task56/Main.java
+++ src/Task56/Main.java
@@ -1,24 +1,24 @@
 package Task56;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("How many numbers ?");
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int arr[]=new int[n];
-		System.out.println("Enter numbers : ");
-		for(int i=0;i<n;i++){
-			System.out.print("number("+i+") : ");
-			int num=sc.nextInt();
-			arr[i]=num;
+		System.out.print("How many strings ?");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextLine();
+		String arr[] = new String[n];
+		for (int i = 0; i < n; i++) {
+			System.out.print("String(" + i + ") : ");
+			String sth = sc.nextLine();
+			arr[i] = sth;
 		}
-for(int i=arr.length-1;i>=0;i--){
-	System.out.print(arr[i]+" | ");
-}
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " | ");
+		}
+		sc.close();
 	}
 
 }
